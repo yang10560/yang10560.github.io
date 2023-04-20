ec994a323d9e1db481bae6960249c9750e2ff023
mryang <yang10560@163.com>
2023-04-20 11:09:17 +0800

    gpt.html

--- js/aichat.js
+++ js/aichat.js
@@ -3,7 +3,7 @@ const sendButton = document.getElementById("send-button");
 const sendButton2 = document.getElementById("send-button2");
 //const inputField = document.querySelector("#input-container input[type='text']");
 const inputField = document.querySelector("#input-container textarea");
-var defualtAPIPUBKEY = "HM82J7CLX8";//默认1
+var defualtAPIPUBKEY = "R9nq274BYb";//默认1
 
 hljs.configure({
 	ignoreUnescapedHTML: true
