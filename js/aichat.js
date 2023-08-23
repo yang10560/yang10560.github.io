16f4e533b9bd94d3e4a77192bf980be10a91264f
mryang <yang10560@163.com>
2023-08-23 10:26:29 +0800

    update web

--- js/aichat.js
+++ js/aichat.js
@@ -709,9 +709,9 @@ function gg() {
 	location.href = location.protocol + "//" + location.host + location.pathname + "?random=" + Math.random();
 
 }
-if (!location.href.includes("random")) {
+if (!location.href.includes("random") && !location.href.includes("antijump")) {
 	//刷新缓存
-	location.href = location.protocol + "//" + location.host + location.pathname + "?random=" + Math.random();
+	location.href = location.protocol + "//" + location.host + location.pathname + "?random=" + Math.random()+"&antijump=" + Math.random();
 }
 
 //载入历史
